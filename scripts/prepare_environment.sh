f16dda2450180bfc6d0ae0eb89323a07ae3afc1e
prasantk <publicapi@live.com>
2017-12-04 18:44:02 +0530

    added code to prepare elasticsearch, prometheus and grafana directory

--- scripts/prepare_environment.sh
+++ scripts/prepare_environment.sh
@@ -3,5 +3,5 @@
 echo -n "Setting up the max map count..."
 echo 'vm.max_map_count=262144'| sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
-echo -n "Setting up the elasticsearch directory..."
-sudo mkdir -p /data/elasticsearch/data && sudo chown -R 1000:1000 /data
+echo -n "Setting up the elasticsearch, prometheus and grafana directory..."
+sudo mkdir -p -- /data/elasticsearch/data /data/prometheus/data /data/grafana/data && sudo chown -R 1000:1000 /data
